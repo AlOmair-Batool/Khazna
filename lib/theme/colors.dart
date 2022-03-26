be9f0d553b987c8d2fd61eba71abf622f073b1a8
HailahAlrashed <90303853+HailahAlrashed@users.noreply.github.com>
2022-03-26 06:11:30 +0300

    Intro page + launcher icon

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-const Color primary = Color(0xFF3CBFA9);
-const Color secondary = Color(0xFF0A967E);
+const Color primary= Color(0xFF40A083);
+const Color secondary = Color(0xFF4D908E);
 const Color black = Color(0xFF000000);
 const Color white = Color(0xFFFFFFFF);
 const Color grey = Colors.grey;
 const Color red = Color(0xFFec5766);
 const Color blue = Color(0xFF0071BC);
-const Color green= Color(0xFF4D908E);
+const Color  green = Color(0xFF3CBFA9);
+
