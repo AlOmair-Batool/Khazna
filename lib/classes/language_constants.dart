ff6df630d3b662e5da2b66492aea287f10587f61
HailahAlrashed <90303853+HailahAlrashed@users.noreply.github.com>
2022-10-29 11:14:09 +0300

    Add logout

--- lib/classes/language_constants.dart
+++ lib/classes/language_constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 const String LAGUAGE_CODE = 'languageCode';
 
 //languages code
