841a5a7eb5271e4ed46c8bb5794423f38af4af89
HailahAlrashed <90303853+HailahAlrashed@users.noreply.github.com>
2022-11-07 22:29:33 +0300

    app.py final update

--- API/app.py
+++ API/app.py
@@ -16,9 +16,6 @@ import gpflow
 from firebase_admin import credentials
 from firebase_admin import firestore
 import firebase_admin
-#import matplotlib.pyplot as plt
-import firebase_admin
-from firebase_admin import credentials
 import json
 import os
 
@@ -28,42 +25,44 @@ app = Flask(__name__)
 
 @app.route('/' ,methods= ['GET','POST'])
 def home():
-    #if request.method == 'POST':
-    current_date = pd.Series([date.today()])
-    current_date = pd.to_datetime(current_date)
-    day_26 = get_day_26_mapping(current_date)[0]
 
-    #model = trained_model()
-    model = pickle.load(open('gp_model.pkl','rb'))
-    predicted = model.predict_y(np.linspace(datetime.timestamp(day_26)/3600, datetime.timestamp(day_26)/3600, 1).reshape(1, 1) )
-    print(predicted[0].numpy()[0][0])
-    print(date.today())
-    return 'Predicted value: '+str(predicted[0].numpy()[0][0])
+    return 'Testing the API...'
 
 @app.route('/api' ,methods= ['GET','POST'])
 def predict_spending():
-    #if request.method == 'POST':
+    
+    request_data = request.get_json()
+    user_id = request_data['userid']
+    
+    #training the model each time with the new dataset and pickle it
+    gp_model(user_id)
+    
     predicted_output = {}
     
     #getting current date
     current_date = pd.Series([date.today()])
     current_date = pd.to_datetime(current_date)
-    day_26 = get_day_26_mapping(current_date)[0]
-    first_day_26 = get_day_26_mapping(current_date)[0]
-    last_day_26 = get_day_26_mapping(current_date,True)[0]
+    
+    #getting the closest 27th
+    first_day_27 = get_day_27_mapping(current_date)[0]
+    last_day_27 = get_day_27_mapping(current_date,True)[0]
 
+    #reading back the pickled model
     model = pickle.load(open('gp_model.pkl','rb'))
-    predicted = model.predict_y(np.linspace(datetime.timestamp(first_day_26)/3600, datetime.timestamp(last_day_26)/3600,24).reshape(24, 1))
+    
+    #predict the balance
+    predicted = model.predict_y(np.linspace(datetime.timestamp(first_day_27)/3600, datetime.timestamp(last_day_27)/3600,24).reshape(24, 1))
     
     predicted_output['output'] = str(sum(predicted[0]).numpy()[0])
     return predicted_output
 
 
-def get_day_26_mapping(date, last=False):
+
+#method that returns the closest 27th date from the received date
+def get_day_27_mapping(date, last=False):
     import datetime
     new_date = []
     for m,d,y in zip(date.dt.month,date.dt.day,date.dt.year):
-        #print(m,d,y)
         new_d = 0
         new_m = 0
         new_y = 0
@@ -88,17 +87,20 @@ def get_day_26_mapping(date, last=False):
             new_date.append(datetime.datetime(new_y,new_m,new_d,0,0,0))
             
         return new_date
-####new 
-        
+
+
+ #predicting the msg type        
 @app.route('/type' ,methods= ['GET','POST'])
 def predict_msg_type():
-    
+    #read the msg
     request_data = request.get_json()
     test_msg1 = request_data['msg']
+    
     predicted_msg_type = {}
-
+    
+    
+    #check the language to run the appropriate model
     if check_language(test_msg1) == 'English':
-        #getting current date
         msg = prepare_all(test_msg1)
 
         model = pickle.load(open('SVM_model.pkl','rb'))
@@ -112,6 +114,8 @@ def predict_msg_type():
 
     return predicted_msg_type
 
+
+#method that checks the language of the sms
 def check_language(msg):
     arr_msg = msg.split(' ')[0];
     regexPattern = "^[A-Za-z0-9]+";
@@ -120,6 +124,7 @@ def check_language(msg):
     return "Arabic"
     
 
+#method that remove any arabic word
 def remove_arabic(msg):
     arr_msg = msg.split(' ');
     regexPattern = "^[A-Za-z0-9]+";
@@ -127,14 +132,19 @@ def remove_arabic(msg):
     space = " "
     return space.join(english_arr_msg)
 
+
+#apply all the preprocessing on the sms msg to be predicted 
+#and return it to be ready for prediction
 def prepare_predict_msg(test_msg):
     transactions = pd.read_csv("newtransactions.csv",encoding='latin-1')
     transactions.columns = transactions.columns.str.replace('ï»¿', '')
 
-    
+    #convert to lower case
     test_msg_lower = test_msg.lower()
+    #remove arabic words
     test_msg_lower_no_arabic =  remove_arabic(test_msg_lower)
     tokenizer = RegexpTokenizer("[\w+|\$[\d\.]+|\S+]+")
+    #tokenize the text
     test_msg_tokenize = tokenizer.tokenize(test_msg_lower_no_arabic) 
 
     nltk.download('wordnet')
@@ -154,14 +164,18 @@ def prepare_predict_msg(test_msg):
 
     return test_msg_tokenize_final
 
+
+#preparing the corpus and doing preprocessing 
 def prepare_all(test_msg):
     transactions = pd.read_csv("newtransactions.csv",encoding='latin-1')
     transactions.columns = transactions.columns.str.replace('ï»¿', '')
 
-    
+    #convert to lower case
     transactions['sms'] = [entry.lower() for entry in transactions['sms']]
+    #remove arabic words
     transactions['sms'] = [remove_arabic(entry) for entry in transactions['sms']]
-    tokenizer = RegexpTokenizer("[\w+|\$[\d\.]+|\S+]+")        
+    tokenizer = RegexpTokenizer("[\w+|\$[\d\.]+|\S+]+")     
+    #tokenize the text
     transactions['sms']= [tokenizer.tokenize(entry) for entry in transactions['sms']]
 
     nltk.download('wordnet')
@@ -173,7 +187,6 @@ def prepare_all(test_msg):
     for index,entry in enumerate(transactions['sms']):
         # Declaring Empty List to store the words that follow the rules for this step
         Final_words = []
-        # Initializing WordNetLemmatizer()
         # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
 
         for word, tag in pos_tag(entry):
@@ -190,10 +203,9 @@ def prepare_all(test_msg):
     return test_msg_Tfidf
    
 
-#plt.rcParams["figure.figsize"] = (12, 6)
 
 
-def get_day_26_mapping_model(date):
+def get_day_27_mapping_model(date):
     import datetime
     new_date = []
     for m,d,y in zip(date.dt.month,date.dt.day,date.dt.year):
@@ -215,19 +227,18 @@ def get_day_26_mapping_model(date):
                 new_d = 26
                 new_m = m + 1
                 new_y = y
-        #print(d,m,y)
-        #print(new_d,new_m,)
+ 
         new_date.append(datetime.datetime(new_y,new_m,new_d))
     return new_date
     
 
+#connecting to the DB and retriveing the user transactions
 def connect_db(user_id):
-    user_id= user_id.lower()
+    
+    #connecting the firebase
     if not firebase_admin._apps:
         cred = credentials.Certificate('sim1-ac95f-firebase-adminsdk-3hy48-80b6b88451.json') 
-        default_app = firebase_admin.initialize_app(cred)
-    
-    
+        default_app = firebase_admin.initialize_app(cred)    
     
     
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sim1-ac95f-firebase-adminsdk-3hy48-80b6b88451.json"
@@ -240,6 +251,7 @@ def connect_db(user_id):
     
     dict_users = {"userid":[],"type":[], "amount":[],"time":[],"date":[]}
     
+    #going through all the records and storing them as a dictionary
     for trans in transaction:
         try:
             us = json.loads(str(trans.to_dict()).replace("'",'"').replace(" ","").lower())
@@ -250,215 +262,36 @@ def connect_db(user_id):
             dict_users["date"].append(us["date"])
         except:
             continue
-    ##
+    
+    #converting the dictionary to Dataframe
+    
     df = pd.DataFrame.from_dict(dict_users)
-  
+    
+    user_id= user_id.lower()
+
     df = df[df["userid"]==user_id]
     
     return df
 
  
- #agg for trans (take abs value after) and return only the last 31 days     
-@app.route('/predict' ,methods= ['GET','POST'])
-def gp_model():
+ 
+def gp_model(user_id):
     
-    request_data = request.get_json()
-    user_id = request_data['userid']
     df = connect_db(user_id)
-
+    #constructing the date from date and time column
     df["date"] = pd.to_datetime(df['date']+" "+df["time"],dayfirst = True,errors='coerce')
     df["amount"] = df["amount"].astype(float)
-    df['day_26_mapping_date'] = get_day_26_mapping_model(df['date'])
+    #creating a new column named day_27_mapping_date
+    #where every date has a mapping day 27 of the closest month
+    df['day_27_mapping_date'] = get_day_27_mapping_model(df['date'])
     
-
+    #make the value of the amount negative if the type is withdrawal
     df['amount'] = tf.cast(df['amount'], tf.float64)
     condition = df["type"] == 'withdrawal'
     df.loc[condition, ['amount']] *= -1
     
-    for x in ['amount']:
-        q75,q25 = np.percentile(df.loc[:,x],[75,25])
-        intr_qr = q75-q25
-     
-        max = q75+(1.5*intr_qr)
-        min = q25-(1.5*intr_qr)
-     
-        df.loc[df[x] < min,x] = np.nan
-        df.loc[df[x] > max,x] = np.nan
-    
-    df = df.dropna(axis = 0)
-    df = df.sort_values(by='date',ascending=True)
-
-    df["unix_date"] = [datetime.timestamp(dt)/3600 for dt in df["date"]]
-    df["unix_date"] =tf.cast(df["unix_date"] , tf.float64)
-    X = df[["unix_date"]]
-    X = tf.cast(X, tf.float64) 
-    
-    Y = df[['amount']]
-    Y = tf.cast(Y, tf.float64) 
-    
-    
-    X_train = np.reshape(X,(-1, 1))
-    Y_train = np.reshape(Y,(-1, 1))
-    
-    k = gpflow.kernels.RBF()
-    m = gpflow.models.GPR(data=(X_train, Y_train), kernel=k, mean_function=None)
- 
-    
-    #creating testing data
-    last_trans = X.numpy().max()
-    # mapping the last date of transaction to day_26_mapping_date column to get day 26
-    day_26 = df[last_trans== df["unix_date"]]['day_26_mapping_date']
- 
-    # converting day_26 to the to unix timestamp
-    day_26_unix_time = datetime.timestamp(pd.to_datetime(day_26.values[0]))/3600
-
-
-    #generates points from the first transaction till  the nearst day 26 of the last transaction
-    X_test = np.linspace(X.numpy().min(), day_26_unix_time , 100).reshape(100, 1) #doxum
-
-    mean, var = m.predict_f(X_test)
-    #generate 10 samples from posterior
-    tf.random.set_seed(1)  # for reproducibility
-
-    X_train = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_train]
-    X_test = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_test]
-    Y_train = [dt[0] for dt in Y_train]
-    mean = [dt[0] for dt in mean.numpy()]
-    
-    
-    #plt.plot(X_train,Y_train)
-
-    
-    #plt.plot(X_test,mean)
-    #plt.savefig('trans.png')
-
-    pickle.dump(m, open('gp_model.pkl', 'wb'))
-    
-    return { "X_train":X_train,
-            "Y_train":Y_train,
-            "X_test":X_test,
-            "mean": mean
-            }
-
-
-#agg for trans (take abs value after) and return only the last 31 days   
-@app.route('/predict_v1' ,methods= ['GET','POST'])
-def gp_model_v1():
-    req_data = request.get_json()
-    id_ = req_data['userid']
-    
-    df = connect_db(id_)
-
-    df["date"] = pd.to_datetime(df['date']+" "+"23:59:59",dayfirst = True,errors='coerce')
-    df["amount"] = df["amount"].astype(float)
-    df['day_26_mapping_date'] = get_day_26_mapping_model(df['date'])
-    
-    
-
-    df['amount'] = tf.cast(df['amount'], tf.float64)
-    condition = df["type"] == 'withdrawal'
-    df.loc[condition, ['amount']] *= -1
-    
-    df=df.groupby(['date','day_26_mapping_date'])['amount'].agg('sum').reset_index()
-    
-    for x in ['amount']:
-        q75,q25 = np.percentile(df.loc[:,x],[75,25])
-        intr_qr = q75-q25
-     
-        max = q75+(1.5*intr_qr)
-        min = q25-(1.5*intr_qr)
-     
-        df.loc[df[x] < min,x] = np.nan
-        df.loc[df[x] > max,x] = np.nan
-    
-    df = df.dropna(axis = 0)
-    df = df.sort_values(by='date',ascending=True)
-
-    df['amount'] = abs(df['amount'])
-
-    df["unix_date"] = [datetime.timestamp(dt)/3600 for dt in df["date"]]
-    df["unix_date"] = tf.cast(df["unix_date"] , tf.float64)
-    X = df[["unix_date"]]
-    X = tf.cast(X, tf.float64) 
-    
-    Y = df[['amount']]
-    Y = tf.cast(Y, tf.float64) 
-    
-    
-    X_train = np.reshape(X,(-1, 1))
-    Y_train = np.reshape(Y,(-1, 1))
-    
-    k = gpflow.kernels.RBF()
-    m = gpflow.models.GPR(data=(X_train, Y_train), kernel=k, mean_function=None)
-     
-    #creating testing data
-    last_trans = X.numpy().max()
-    # mapping the last date of transaction to day_26_mapping_date column to get day 26
-    day_26 = df[last_trans== df["unix_date"]]['day_26_mapping_date']
- 
-    # converting day_26 to the to unix timestamp
-    day_26_unix_time = datetime.timestamp(pd.to_datetime(str(day_26.values[0])+" "+"23:59:59",errors='coerce'))/3600
-
-    
-
-    #generates points from the first transaction till  the nearst day 26 of the last transaction
-    X_test = np.linspace(X.numpy().min(), day_26_unix_time , 100).reshape(100, 1) #doxum
-
-    mean, var = m.predict_f(X_test)
-    #generate 10 samples from posterior
-    tf.random.set_seed(1)  # for reproducibility
- 
-    X_train = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_train]
-    #X_train_ts = [pd.to_datetime(dt) for dt in  X_train]
-
-    Y_train = [dt[0] for dt in  Y_train]
-    X_test = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_test]
-    #X_test_ts  = [pd.to_datetime(dt) for dt in  X_test]
-
-    mean_list = [dt[0] for dt in mean.numpy()]
-    
-    data = {'X_test': X_test, 'mean': mean_list}  
-    df_pred = pd.DataFrame(data)
-    
-    df_pred= df_pred.groupby(['X_test'])['mean'].agg('sum').reset_index()
-    df_pred = df_pred[["X_test","mean"]][-32:]
-    
-    #plt.plot(df_pred['X_test'],df_pred['mean'])
-    
-    pickle.dump(m, open('gp_model.pkl', 'wb'))
-    #plt.savefig('trans.png')
-
-    return { "X_train":X_train,
-             #"X_train_ts":X_train_ts,
-            "Y_train":Y_train,
-            "X_test":df_pred["X_test"].tolist(),
-            #"X_test_ts":df_pred["X_test_ts"].tolist(),
-            "mean": df_pred["mean"].tolist()
-            }
-
-
-
-#agg for trans (- and + ) and return only the last 31 days
-@app.route('/predict_v2' ,methods= ['GET','POST'])
-def gp_model_v2():
-    
-
-
-    request_data = request.get_json()
-    user_id= request_data['userid']
-    df = connect_db(user_id)
-
-    df["date"] = pd.to_datetime(df['date']+" "+"23:59:59", dayfirst = True,errors='coerce')
-    df["amount"] = df["amount"].astype(float)
-    df['day_26_mapping_date'] = get_day_26_mapping_model(df['date'])
-        
-    
-    df['amount'] = tf.cast(df['amount'], tf.float64)
-    condition = df["type"] == 'withdrawal'
-    df.loc[condition, ['amount']] *= -1
-    
-    df=df.groupby(['date','day_26_mapping_date'])['amount'].agg('sum').reset_index()
-    
+    #removing outliers since GP is sensitive to outliers
+    #by filling them with nan and the dropping them
     for x in ['amount']:
         q75,q25 = np.percentile(df.loc[:,x],[75,25])
         intr_qr = q75-q25
@@ -474,119 +307,44 @@ def gp_model_v2():
     df = df.sort_values(by='date',ascending=True)
 
 
+    #creating unix_date from the date column
     df["unix_date"] = [datetime.timestamp(dt)/3600 for dt in df["date"]]
     df["unix_date"] =tf.cast(df["unix_date"] , tf.float64)
     
-    
     X = df[["unix_date"]]
     X = tf.cast(X, tf.float64) 
-    
     Y = df[['amount']]
     Y = tf.cast(Y, tf.float64) 
     
-    
+    #creating the training dataset
+
     X_train = np.reshape(X,(-1, 1))
     Y_train = np.reshape(Y,(-1, 1))
     
+    #creating the model
     k = gpflow.kernels.RBF()
     m = gpflow.models.GPR(data=(X_train, Y_train), kernel=k, mean_function=None)
-    
-    #creating testing data
-    last_trans = X.numpy().max()
-    # mapping the last date of transaction to day_26_mapping_date column to get day 26
-    day_26 = df[last_trans== df["unix_date"]]['day_26_mapping_date']
  
-    # converting day_26 to the to unix timestamp
-    day_26_unix_time = datetime.timestamp(pd.to_datetime(str(day_26.values[0])+" "+"23:59:59",dayfirst = True,errors='coerce'))/3600
+    
+    #creating testing dataset
+    last_trans = X.numpy().max()
+    # mapping the last date of transaction to day_27_mapping_date column to get day 27
+    day_27 = df[last_trans== df["unix_date"]]['day_27_mapping_date']
+ 
+    # converting day_27 to the to unix timestamp
+    day_27_unix_time = datetime.timestamp(pd.to_datetime(day_27.values[0]))/3600
 
 
-    #generates points from the first transaction till  the nearst day 26 of the last transaction
-    X_test = np.linspace(X.numpy().min(), day_26_unix_time , 100).reshape(100, 1) #doxum
+    #generates points from the first transaction till  the nearst day 27 of the last transaction
+    X_test = np.linspace(X.numpy().min(), day_27_unix_time , 100).reshape(100, 1) #doxum
 
     mean, var = m.predict_f(X_test)
-    #generate 10 samples from posterior
-    tf.random.set_seed(1)  # for reproducibility
-    
-    X_train = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_train]
-    #X_train_ts = [pd.to_datetime(dt) for dt in  X_train]
 
-    Y_train = [dt[0] for dt in  Y_train]
-    X_test = [datetime.utcfromtimestamp(dt[0]*3600).strftime('%Y-%m-%d') for dt in  X_test]
-    #X_test_ts  = [pd.to_datetime(dt) for dt in  X_test]
-
-    mean_list = [dt[0] for dt in mean.numpy()]
-    
-    data = {'X_test': X_test, 'mean': mean_list}  
-    df_pred = pd.DataFrame(data)
-    
-    df_pred= df_pred.groupby(['X_test'])['mean'].agg('sum').reset_index()
-    df_pred = df_pred[["X_test","mean"]][-32:]
-    
-    #plt.plot(df_pred['X_test'],df_pred['mean'])
-
+    #picklized the model
     pickle.dump(m, open('gp_model.pkl', 'wb'))
-    #plt.savefig('trans.png')
+    
 
-    
-    return { "X_train":X_train,
-            "Y_train":Y_train,
-            "X_test":df_pred["X_test"].tolist(),
-            "mean": df_pred["mean"].tolist()
-            }
 
-# getting all trans type withdrawls + deposit
-@app.route('/predict_v3' ,methods= ['GET','POST'])
-def gp_model_v3():
-       
-    
-    request_data = request.get_json()
-    user_id= request_data['userid']
-    df = connect_db(user_id)
-
-    df["amount"] = df["amount"].astype(float)
-    df['date'] = pd.to_datetime(df['date'],dayfirst = True)
-
-    
-    df['amount'] = tf.cast(df['amount'], tf.float64)
-    condition = df["type"] == 'withdrawal'
-    df.loc[condition, ['amount']] *= -1
-    
-    df=df.groupby(['date'])['amount'].agg('sum').reset_index()
-    df = df.sort_values(by='date',ascending=True)
-    df['date'] = df['date'].dt.strftime('%Y-%m-%d')
-    #plt.plot(df['date'][-10:],df['amount'][-10:])
-    #plt.savefig('trans.png')
-
-    return {"date":df['date'].tolist(),
-            "amount":df['amount'].tolist()}
-
-#getting withdrawal trans only
-@app.route('/predict_v4' ,methods= ['GET','POST'])
-def gp_model_v4():
-    
-    request_data = request.get_json()
-    user_id= request_data['userid']
-    df = connect_db(user_id)
-
-    df["amount"] = df["amount"].astype(float)
-    df['date'] = pd.to_datetime(df['date'],dayfirst = True)
-    
-    df['amount'] = tf.cast(df['amount'], tf.float64)
-    condition = df["type"] == 'withdrawal'
-    df = df[condition]
-    
-    df=df.groupby(['date'])['amount'].agg('sum').reset_index()
-    print(df)
-
-    df = df.sort_values(by='date',ascending=True)
-    df['date'] = df['date'].dt.strftime('%Y-%m-%d')
-    
-    #plt.plot(df['date'][-10:],df['amount'][-10:])
-    #plt.savefig('trans.png')
-    
-    return {"date":df['date'].tolist(),
-            "amount":df['amount'].tolist()}
-    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
 
